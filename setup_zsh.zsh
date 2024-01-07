b516e7a19b9b1b9e4b05270dc14cffc180cfdd35
Jimmy Nielsen <9019125+Jimmysnielsen@users.noreply.github.com>
2024-01-07 10:21:39 +0100

    adding setup_zsh.zsh
    changing default shell to /usr/local/bin/zsh

---
+++ setup_zsh.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting zsh Setup >>>\n"
+
+
+# installation unnecessary - it is in the Brewfile
+
+echo "Enter superuser (sudo) password to edit /etc/shells"
+echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells'
+
+echo "Enter user password to change login shell"
+chsh -s '/usr/local/bin/zsh'
+
+
